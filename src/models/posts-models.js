9051db20edd98144cbebeec227c0f573b85fdc42
William Kunitake <william.kunitake@gmail.com>
2024-11-26 11:09:02 -0800

    Adicionando o novo post à collection "posts" do MongoDB

--- src/models/posts-models.js
+++ src/models/posts-models.js
@@ -1,14 +1,20 @@
 import conectarAoBanco from "../config/db-config.js"; // Importa a função para conectar ao banco de dados
 
 // Conecta ao banco de dados usando a string de conexão fornecida pela variável de ambiente STRING_CONEXAO
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO); // Estabelece a conexão com o banco de dados MongoDB usando a string de conexão obtida da variável de ambiente.
 
 // Função assíncrona para obter todos os posts do banco de dados
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     // Seleciona o banco de dados "imersao-instabyte"
-    const db = conexao.db("imersao-instabyte");
+    const db = conexao.db("imersao-instabyte"); // Obtém uma referência ao banco de dados específico.
     // Seleciona a coleção "posts" dentro do banco de dados
-    const colecao = db.collection("posts");
+    const colecao = db.collection("posts"); // Obtém uma referência à coleção "posts" dentro do banco de dados.
     // Executa uma consulta para encontrar todos os documentos na coleção e retorna os resultados como um array
-    return colecao.find().toArray();
+    return colecao.find().toArray(); // Executa uma consulta para encontrar todos os documentos na coleção e retorna os resultados em um array.
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-instabyte"); // Obtém uma referência ao banco de dados específico.
+    const colecao = db.collection("posts"); // Obtém uma referência à coleção "posts" dentro do banco de dados.
+    return colecao.insertOne(novoPost); // Insere um novo documento (o post) na coleção "posts".
 }
